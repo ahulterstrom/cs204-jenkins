d475700bdbf6b969b779b9bd2677eee79effd842
ahulterstrom <andrewhulterstrom@gmail.com>
2022-01-17 14:30:04 -0700

    implement createUniqueID

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -66,7 +67,7 @@ class Calculator {
      */
     String createUniqueID(String n){
 
-        return null;
+        return n + UUID.randomUUID();
     }
 
 
