237c40bfc6ccfc496e2e3b79da1e170d975873ca
ahulterstrom <andrewhulterstrom@gmail.com>
2022-01-17 22:18:18 -0700

    create intentional test failure

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -40,9 +40,10 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        double phi = (1 + Math.sqrt(5)) / 2;
-        return (int) Math.round(Math.pow(phi, n)
-                / Math.sqrt(5));
+        return Integer.parseInt("on no failure");
+//        double phi = (1 + Math.sqrt(5)) / 2;
+//        return (int) Math.round(Math.pow(phi, n)
+//                / Math.sqrt(5));
     }
 
 
