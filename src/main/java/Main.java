d5e0ee55255c5a10dd1e0980cda727f8a44e20d8
ahulterstrom <andrewhulterstrom@gmail.com>
2022-01-17 23:15:41 -0700

    make Main run from System.in instead of arguments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,53 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Check how many arguments were passed in
-        if(args.length == 0)
-        {
-            System.out.println("Too few arguments");
-            System.exit(0);
-        }
-        String operation = args[0];
-        int x = Integer.parseInt(args[1]);
-        int y = 0;
-        if(args.length == 3){
-            y = Integer.parseInt(args[2]);
-        }
-        String z;
 
-        Calculator calculator = new Calculator();
+        Scanner scanner=new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line=scanner.nextLine();
+            String[] inputArray=line.trim().split("\\s+");
+
+            // Check how many arguments were passed in
+            if (inputArray.length == 0) {
+                System.out.println("Too few arguments");
+            }
+            String operation=inputArray[0];
+            int x=Integer.parseInt(inputArray[1]);
+            int y=0;
+            if (inputArray.length == 3) {
+                y=Integer.parseInt(inputArray[2]);
+            }
+            String z;
+
+            Calculator calculator=new Calculator();
 
 
-        switch (operation) {
-            case "add":
-                z=Integer.toString(calculator.add(x, y));
-                break;
-            case "subtract":
-                z=Integer.toString(calculator.subtract(x, y));
-                break;
-            case "multiply":
-                z=Integer.toString(calculator.multiply(x, y));
-                break;
-            case "divide":
-                z=Integer.toString(calculator.divide(x, y));
-                break;
-            case "fibonacci":
-                z=Integer.toString(calculator.fibonacciNumberFinder(x));
-                break;
-            case "binary":
-                z=calculator.intToBinaryNumber(x);
-                break;
-            default:
-                throw new Error("Operator not recognized");
+            switch (operation) {
+                case "add":
+                    z=Integer.toString(calculator.add(x, y));
+                    break;
+                case "subtract":
+                    z=Integer.toString(calculator.subtract(x, y));
+                    break;
+                case "multiply":
+                    z=Integer.toString(calculator.multiply(x, y));
+                    break;
+                case "divide":
+                    z=Integer.toString(calculator.divide(x, y));
+                    break;
+                case "fibonacci":
+                    z=Integer.toString(calculator.fibonacciNumberFinder(x));
+                    break;
+                case "binary":
+                    z=calculator.intToBinaryNumber(x);
+                    break;
+                default:
+                    throw new Error("Operator not recognized");
+            }
+            System.out.println(z);
         }
-        System.out.println(z);
     }
 
 }
